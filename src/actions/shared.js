38eef6d6ae51c39707c3ea2180f06b3ae9f4ea81
mode-x <abiaemma@gmail.com>
2022-07-25 09:16:32 +0100

    refactor: Add authed user action

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,15 @@
 import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweet";
 import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return async (dispatch) => {
     const { users, tweets } = await getInitialData();
     dispatch(receiveUsers(users));
     dispatch(receiveTweets(tweets));
+    dispatch(setAuthedUser(AUTHED_ID));
   };
 }
