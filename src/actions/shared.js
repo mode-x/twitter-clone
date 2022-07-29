47f574ee7eaa80c6c5c48056185fd526b71b5c6b
mode-x <abiaemma@gmail.com>
2022-07-29 09:40:01 +0100

    feature: Add Tweet

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,14 +2,17 @@ import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
 import { setAuthedUser } from "./authedUser";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
 
 const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return async (dispatch) => {
+    dispatch(showLoading());
     const { users, tweets } = await getInitialData();
     dispatch(receiveUsers(users));
     dispatch(receiveTweets(tweets));
     dispatch(setAuthedUser(AUTHED_ID));
+    dispatch(hideLoading());
   };
 }
